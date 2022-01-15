7454c6bb5dc773e00fbcb415253e59c5b7614879
HyeonE99 <jaehyeon0606@naver.com>
2022-01-15 17:35:23 +0900

    app.js 파일을 수정함

--- hi_bye/src/App.js
+++ hi_bye/src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi
         </p>
         <a
           className="App-link"
